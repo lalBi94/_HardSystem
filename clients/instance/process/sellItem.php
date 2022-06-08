af77c497cd9dc749a9cb98c38c5e7ff5b56b0132
pro.boooooo <pro.boudjemline@gmail.com>
2022-06-08 12:13:39 +0200

    $

--- clients/instance/process/sellItem.php
+++ clients/instance/process/sellItem.php
@@ -6,27 +6,106 @@
         die;
     }
 
-    //cote entreprise / formulaire
-    @$id_buy = $_GET['id'];
-    @$qte = $_GET['qte'];
-    @$cgu = $_GET['CGU'];
+    //VARIABLE : cote entreprise / formulaire
+    @$id_buy = $_GET['id']; //id de la demande
+    @$qte = $_GET['qte']; //quantite 
+    
+    $price_req = mysqli_query($db, "select price from businessbuy where id='$id_buy'");
+    $price_fetch = mysqli_fetch_assoc($price_req);
+    $price = $price_fetch['price']; //prix
+
     $item_req = mysqli_query($db, "select typeItem from businessbuy where id='$id_buy'");
     $item_fetch = mysqli_fetch_assoc($item_req);
-    $item = $item_fetch['typeItem'];
-    $price = $_GET['price'];
+    $item = $item_fetch['typeItem']; //id de l'item
 
-    //cote client
+    //VARIABLE : cote client
     $id_client = $_SESSION['id_client'];
     $stash_req = mysqli_query($db, "select stash from customer where id='$id_client'");
     $stash_fetch = mysqli_fetch_assoc($stash_req);
     $stash = $stash_fetch['stash'];
 
+    //INSERTION dans customersell
     $insertIntoClient_req = mysqli_query($db, "insert into customersell(client, item, price, quantity, date_sell, time_sell) values('$id_client', '$item', $price,'$qte', current_date(), current_time())");
     if(!$insertIntoClient_req){
-        echo "Erreur";
+        echo "Erreur dans issert. custommersell";
         die;
-    } else{
-        echo "Vendu avec succes !";
+    } 
+
+    //INSERTION nouveau stock 
+    $qteStock_req = mysqli_query($db, "select quantity from businessbuy where id='$id_buy' and typeItem='$item'");
+    if(!$qteStock_req){
+        echo "probleme requete stock";
+        die;
     }
-    die;
+    $qteStock_fetch = mysqli_fetch_assoc($qteStock_req);
+    $qteStock = $qteStock_fetch['quantity'];
+    
+    $newStock = $qteStock - $qte;
+    
+    $modifBusinessBuy = mysqli_query($db, "update businessbuy set quantity='$newStock' where id='$id_buy'");
+    if(!$modifBusinessBuy){
+        echo "probleme lors de la modif de la bdd";
+        die;
+    }
 ?>
+
+<html>
+    <head>
+    <link rel='stylesheet' href='../../../style/all.css'>
+        <link rel='stylesheet' href='../../../style/nav.css'>
+    </head>
+
+    <body>
+        <style>
+            main #whenRequestSend{
+                margin: 0 auto;
+                margin-top: 4%;
+                width: 61rem;
+                font-size: 2rem;
+                text-align: center;
+                font-family: 'Courier New', Courier, monospace;
+            }
+
+            main .failure_msg{
+                margin-top: 2vw;
+            }
+
+            main .homepage_return{
+                border: 1px solid #0a1b2f; 
+                border-radius: 5px;
+                background: #0a1b2f;
+                padding: 9px; 
+                text-decoration: none;
+                color: white;
+                margin-top: 10%;
+            }
+
+            main .gif_request{
+                width: 150px;
+                height: auto;
+            }
+        </style>
+
+        <header>
+            <nav id="nav-container">
+                <ul id="nav-menu-container">
+                    <a class ="nav-item" href="../index_instance.php"><img class="image-logo" src="../../../assets/logos/nav-con.png"></a>
+                </ul>
+            </nav>
+        </header>
+
+        <main>
+            <?php
+                echo "<div id='whenRequestSend'>";
+                echo "<img class='gif_request' src='../../../assets/request_send/check_request.gif' alt='Request send !'></img>";
+                echo "<p class='failure_msg'>Votre objet a bien ete vendu !<br><br>";
+                echo "<div class='homepage_link'>";
+                echo "<form>";
+                echo "<button class='btn-request' formaction='../index_instance.php'>Revenir a l'accueil</button>";
+                echo "</form>";
+                echo "</div>";
+            ?>
+        </main>
+    </body>
+</html>
+
--- clients/instance/sell_instance.php
+++ clients/instance/sell_instance.php
@@ -67,6 +67,10 @@
                 color: white;
             }
 
+            .text-cgu{
+                font-size: 18px; 
+            }
+
         </style>
 
         <?php require('./require_nav.php'); ?>
@@ -84,26 +88,29 @@
                 <?php //Afficher les items du site
                     if(isset($_GET['item']) && !empty($_GET['item']) && $_GET['item'] != "" && $_GET['item'] != " "){
                         @$get = $_GET['item'];
-                        $displayitem = mysqli_query($db, "select I.name, I.price, BB.id, P.url, BB.quantity from typeitem I, picture P, businessbuy BB, business B where I.name like '%$get%' and BB.typeItem = I.id and B.id = BB.business and P.item = BB.typeItem");
-                        $displayitem2 = mysqli_query($db, "select I.name, I.price, BB.id, P.url, BB.quantity from typeitem I, picture P, businessbuy BB, business B where I.name like '%$get%' and BB.typeItem = I.id and B.id = BB.business and P.item = BB.typeItem");
+                        $displayitem = mysqli_query($db, "select I.name, I.price, BB.id, BB.typeItem, P.url, BB.quantity from typeitem I, picture P, businessbuy BB, business B where I.name like '%$get%' and BB.typeItem = I.id and B.id = BB.business and P.item = BB.typeItem");
                         $displaybusiness = mysqli_query($db, "select B.name from typeitem I, picture P, businessbuy BB, business B where I.name like '%$get%' and BB.typeItem = I.id and B.id = BB.business and P.item = BB.typeItem");
                         $nb = mysqli_num_rows($displayitem);
 
-                        if($nb > 0){
+                        if($nb == 0 || empty($get) || !isset($get) || $get == null || $get == " " || $get == ""){
+                            echo "<p style='color: red'>Aucun resultat !</p><br>";
+                            die;
+                        } if($nb > 0){
                             echo "<p style='color: green'>Disponible ! ($nb resultat";
-                        if($nb > 1){
+                        } if($nb > 1){
                             echo "s)</p>";
                         } else{
                             echo ")</p>";
                         } 
 
                         $item = (int) getItemId($get);
-                        
+
                         while($fetch = mysqli_fetch_assoc($displayitem) and $fetch2 = mysqli_fetch_assoc($displaybusiness)){
                             $initprice = $fetch['price'];
                             $price1 = ($initprice*40)/100;
                             $price = $initprice-$price1;
                             $id = $fetch['id'];
+                            $idItem = $fetch['typeItem'];
 
                             // $cagnotte_price = mysqli_query($db, "select E.element, E.quantity, C.qteMG, C.price from cagnotte C, extractionfromtypeitem E where E.typeitem = '$item' and C.idElem = E.element");
                             // $q = $fetch3['quantity'];
@@ -111,40 +118,38 @@
                             // $add = $q*$p;
 
                             echo "<div style='border: 1px solid black; margin-bottom: 2%;'></div>";
-                            echo "<input style='float: left;' type='radio' name='id' value='$id'>"; //contient id de 
+
+                            echo "<div>";
+                            echo "<input style='float: left;' type='radio' name='id' value='$id'>"; //contient id de la demande
                             echo "<p style='font-weight: bold; margin-left: 5%;'>".$fetch['name']."</p><br>";
                             echo "<img style='width: 15%; height: auto; margin-left: 3%; margin-bottom: 2%;' src='".$fetch['url']."'>";
-                            echo "<input type='hidden' name='price' value='$price'>";
                             echo "<p style='float: right; text-align: right;'>".
                             "<b>".$price."</b>".
                             "€<br>Acheteur <b>".$fetch2['name']."</b>".
                             "<br>Quantite : "."<b>".$fetch['quantity']."</b>".
                             "</p><br>";
-                        }
-
-                        } if($nb == 0){
-                            echo "<p style='color: red'>Aucun resultat !</p><br>";
+                            echo "</div>";
                         }
 
                         echo "<div style='border: 1px solid black;'></div><br>";
+
+                        echo "<p style='margin-top: 2%; font-weight: bold;'>Quantite :</p>";
+                        echo "<input style='width: 55px; text-align: center;' min='1' class='fields-box-email' type='number' name='qte' value='1'><br><br>";
+    
+                        echo "<p style='margin-top: 2%; font-weight: bold;'>Nom Prenom:</p> <!-- factice -->";
+                        echo "<input class='fields-box-email' type='text'><br>";
+    
+                        echo "<p style='margin-top: 2%; font-weight: bold;'>Adresse :</p> <!-- factice -->";
+                        echo "<input class='fields-box-email' type='text'><br>";
+    
+                        echo "<p style='margin-top: 5%; font-weight: bold;' class='adresse'><b><br>L'appareil devra etre envoyer a l'adresse suivante <br>Il sera envoye a l'entreprise par nos propres soins<br><br><span class='ad'><a href='https://goo.gl/maps/Lqac8fMbmzoD4d8AA' target='_blank'>36 Rue Georges Charpak, 77127 Lieusaint</a></span></b><br><br></p><br>";
+    
+                        echo "<input id='cgu' class='checkbox' type='checkbox' name='CGU'>";
+                        echo "<label class='text-cgu' for='CGU'><b> Accepter les <a href='../../assets/cgu/CGU-Hard-System.fr.pdf' style='color: #0a1b2f;'>CGU</a></b></label><br><br><br>";
+                        echo "<input class='btn-request' style='margin-top: 5%;' type='submit' value='Vendre !'>";
+                        echo "</form>";
                     }
                 ?>
-
-                <p style='margin-top: 2%; font-weight: bold;'>Quantite :</p>
-                <input style='width: 55px; text-align: center;' min='1' class='fields-box-email' type='number' name='qte' value='1'><br><br>
-
-                <p style='margin-top: 2%; font-weight: bold;'>Nom Prenom:</p> <!-- factice -->
-                <input class='fields-box-email' type='text'><br>
-
-                <p style='margin-top: 2%; font-weight: bold;'>Adresse :</p> <!-- factice -->
-                <input class='fields-box-email' type='text'><br>
-
-                <p style='margin-top: 5%; font-weight: bold;' class='adresse'><b><br>L'appareil devra etre envoyer a l'adresse suivante <br>Il sera envoye a l'entreprise par nos propres soins<br><br><span class='ad'><a href='https://goo.gl/maps/Lqac8fMbmzoD4d8AA' target="_blank">36 Rue Georges Charpak, 77127 Lieusaint</a></span></b><br><br></p><br>
-
-                <input id="cgu" class="checkbox" type="checkbox" name="CGU">
-                <label class="text" for="CGU"><b>Accepter les <a href="../../assets/cgu/CGU-Hard-System.fr.pdf" style="color: #0a1b2f;">CGU</a></b></label><br><br><br>
-                <input class='btn-request' style='margin-top: 5%;' type='submit' value='Vendre !'>
-            <form>
         </div>
     </body>
 </html>
